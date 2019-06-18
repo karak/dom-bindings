7ee3a323b18493d2f4e89e6eac3f1961096b4079
karak <karak97@gmail.com>
2019-06-18 17:24:07 +0900

    feat(virtual): add a test case using <virtual> with `if`.

--- test/bindings/virtual.spec.js
+++ test/bindings/virtual.spec.js
@@ -24,6 +24,29 @@ function createSimpleTemplate() {
   }])
 }
 
+function createWithIfTemplate() {
+  return template('<virtual expr0><p expr1><!----></p></virtual>', [{
+    type: bindingTypes.VIRTUAL,
+    selector: '[expr0]',
+    bindings: [
+      {
+        // no selector, which means root element
+        type: bindingTypes.IF,
+        evaluate: scope => scope.isVisible,
+        template: template('<!---->', [])
+      },
+      {
+        selector: '[expr1]',
+        expressions: [{
+          type: expressionTypes.TEXT,
+          childNodeIndex: 0,
+          evaluate: scope => scope.text
+        }]
+      }
+    ]
+  }])
+}
+
 describe('virtual bindings', () => {
   it('virtual bindings without markup will be removed', () => {
     const target = document.createElement('div')
@@ -52,6 +75,32 @@ describe('virtual bindings', () => {
 
     el.update({text: 'goodbye'})
 
+    expect(target.querySelector('p').textContent).to.equal('goodbye')
+
+    el.unmount()
+  })
+
+  it('virtual with if directive binding can be mounted and updated', () => {
+    const target = document.createElement('div')
+
+    const el = createWithIfTemplate().mount(target, {
+      text: 'hello',
+      isVisible: true
+    })
+
+    expect(target.querySelector('virtual')).to.be.not.ok
+    expect(target.querySelector('p')).to.be.ok
+    expect(target.querySelector('p').textContent).to.equal('hello')
+
+    el.update({isVisible: false})
+
+    expect(target.querySelector('virtual')).to.be.not.ok
+    expect(target.querySelector('p')).to.be.not.ok
+
+    el.update({isVisible: true, text: 'goodbye'})
+
+    expect(target.querySelector('virtual')).to.be.not.ok
+    expect(target.querySelector('p')).to.be.ok
     expect(target.querySelector('p').textContent).to.equal('goodbye')
 
     el.unmount()
